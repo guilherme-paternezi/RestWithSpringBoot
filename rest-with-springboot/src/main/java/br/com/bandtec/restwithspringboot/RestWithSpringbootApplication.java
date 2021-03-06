5e4b87d331a80f904be27277e1fb18440a13854d
guilherme-paternezi <guilherme.paternezi@bandtec.com.br>
2021-03-06 12:39:09 -0300

    Adding

---
+++ rest-with-springboot/src/main/java/br/com/bandtec/restwithspringboot/RestWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.restwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringbootApplication.class, args);
+	}
+
+}
---
+++ rest-with-springboot/src/test/java/br/com/bandtec/restwithspringboot/RestWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bandtec.restwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
