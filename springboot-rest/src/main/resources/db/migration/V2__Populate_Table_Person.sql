218d2cc22c05515121f08e2e69dcf782483dc3cc
guilherme-paternezi <guilherme.paternezi@bandtec.com.br>
2021-03-30 14:38:30 -0300

    Flyway

---
+++ springboot-rest/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` VALUES
+    (1,'Uberlandia - Minas Gerais - Brasil','teste','female','teste'),
+    (2,'São Caetano do Sul - São Paulo - Brasil','teste2','female','teste2'),
+    (3,'São Caetano do Sul - São Paulo - Brasil','teste3','male','teste3'),
+    (4,'São Caetano do Sul - São Paulo - Brasil','teste4','female','teste4'),
+    (5,'São Caetano do Sul - São Paulo - Brasil','Pedro','female','Paulo'),
+    (6,'São Caetano do Sul - São Paulo - Brasil','Marco','male','Paulo');
